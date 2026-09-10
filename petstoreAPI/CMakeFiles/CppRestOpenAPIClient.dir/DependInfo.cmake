cb4538d0d4ac1059349527f7cf768e5f18a51de5
wchancock7 <wchancock7@gmail.com>
2026-09-09 20:57:25 -0400

    Add petstoreAPI

---
+++ petstoreAPI/CMakeFiles/CppRestOpenAPIClient.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/OzzieT7/APIs/src/AnyType.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/AnyType.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/AnyType.cpp.o.d"
+  "/home/OzzieT7/APIs/src/ApiClient.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/ApiClient.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/ApiClient.cpp.o.d"
+  "/home/OzzieT7/APIs/src/ApiConfiguration.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/ApiConfiguration.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/ApiConfiguration.cpp.o.d"
+  "/home/OzzieT7/APIs/src/ApiException.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/ApiException.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/ApiException.cpp.o.d"
+  "/home/OzzieT7/APIs/src/HttpContent.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/HttpContent.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/HttpContent.cpp.o.d"
+  "/home/OzzieT7/APIs/src/JsonBody.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/JsonBody.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/JsonBody.cpp.o.d"
+  "/home/OzzieT7/APIs/src/ModelBase.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/ModelBase.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/ModelBase.cpp.o.d"
+  "/home/OzzieT7/APIs/src/MultipartFormData.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/MultipartFormData.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/MultipartFormData.cpp.o.d"
+  "/home/OzzieT7/APIs/src/Object.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/Object.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/Object.cpp.o.d"
+  "/home/OzzieT7/APIs/src/api/PetApi.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/api/PetApi.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/api/PetApi.cpp.o.d"
+  "/home/OzzieT7/APIs/src/api/StoreApi.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/api/StoreApi.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/api/StoreApi.cpp.o.d"
+  "/home/OzzieT7/APIs/src/api/UserApi.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/api/UserApi.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/api/UserApi.cpp.o.d"
+  "/home/OzzieT7/APIs/src/model/ApiResponse.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/ApiResponse.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/ApiResponse.cpp.o.d"
+  "/home/OzzieT7/APIs/src/model/Category.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Category.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Category.cpp.o.d"
+  "/home/OzzieT7/APIs/src/model/Order.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Order.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Order.cpp.o.d"
+  "/home/OzzieT7/APIs/src/model/Pet.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Pet.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Pet.cpp.o.d"
+  "/home/OzzieT7/APIs/src/model/Tag.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Tag.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/Tag.cpp.o.d"
+  "/home/OzzieT7/APIs/src/model/User.cpp" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/User.cpp.o" "gcc" "CMakeFiles/CppRestOpenAPIClient.dir/src/model/User.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
